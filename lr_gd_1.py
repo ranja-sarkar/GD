d2beec67c5b7e2d2738c4ef0eef594339e794952
Ranja <101544669+ranja-sarkar@users.noreply.github.com>
2024-12-07 17:13:53 +0530

    Update

--- lr_gd_1.py
+++ lr_gd_1.py
@@ -1,4 +1,6 @@
-#MODEL 1 for learning rate = 0.05
+# Model 1 for learning rate = 0.05
+# Model 2 for learning rate = 0.2
+# Model 3 for learning rate = 0.0001
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -29,16 +31,17 @@ X = np.array(sorted(list(range(5))*20)) + np.random.normal(size = 100, scale = 0
 y = np.array(sorted(list(range(5))*20)) + np.random.normal(size = 100, scale = 0.3)
 
 
-Clf_1 = GDLinearRegression(learning_rate = 0.05, epoch = 1000)
-Clf_1.fit(X, y)
-y_pred = Clf_1.predict(X)
-mse_1 = mean_squared_error(y, y_pred)
+Clf = GDLinearRegression(learning_rate = 0.05, epoch = 1000)   ## train models 2 & 3 similarly, with diff. learning rates
+Clf.fit(X, y)
+y_pred = Clf.predict(X)
+mse = mean_squared_error(y, y_pred)
+
 plt.style.use('fivethirtyeight')
 plt.scatter(X, y, color='black')
 plt.plot(X, y_pred)
 plt.gca().set_title("Linear Regression Model 1")
 
-print('RMSE = ', round(sqrt(mse_1), 2))
+print('RMSE = ', round(sqrt(mse), 2))
 
 
 
